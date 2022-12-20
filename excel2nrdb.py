d3f82da68aeabb467209594931ab2de3ec63c17b
Peter Marelas <pmarelas@gmail.com>
2022-12-20 12:56:48 +1100

    Chunk file upload to satisfy 1MB API POST limit

--- excel2nrdb.py
+++ excel2nrdb.py
@@ -3,6 +3,7 @@ import click
 import requests
 import datetime
 import gzip
+import math
 
 @click.command()
 @click.option('--excel', type=str, required=True, help='Path to excel file.')
@@ -18,18 +19,25 @@ def main(excel, sheet, event, api, accountid, date):
     df.columns = df.columns.str.replace(' ', '_')
     df['timestamp'] = timestamp
     df['eventType'] = event
-    compressed_payload = gzip.compress(df.fillna("none").to_json(orient="records").encode('utf-8'))
-    session = requests.Session()
-    session.headers.update({
-        "Api-Key": api,
-        "Content-Type": "application/json",
-        "Content-Encoding": "gzip"
-    })
-    url = f"https://insights-collector.newrelic.com/v1/accounts/{accountid}/events"
+    df.fillna("none", inplace=True)
+    json_one_size = len(gzip.compress(df.head(1).to_json(orient="records").encode('utf-8')))
+    rows_per_payload = int(1000000 / json_one_size*0.8)
+    total_rows = df.shape[0]
+    for start in range(0, total_rows, rows_per_payload):
+        stop = start+rows_per_payload
+        print(f"Sending rows from {start}")
+        compressed_payload = gzip.compress(df.iloc[start:stop].to_json(orient="records").encode('utf-8'))
+        session = requests.Session()
+        session.headers.update({
+            "Api-Key": api,
+            "Content-Type": "application/json",
+            "Content-Encoding": "gzip"
+        })
+        url = f"https://insights-collector.newrelic.com/v1/accounts/{accountid}/events"
+        response = session.post(url, data = compressed_payload)
+        response.raise_for_status()
+        print(f"Response status {response.status_code}")
 
-    response = session.post(url, data = compressed_payload)
-    response.raise_for_status()
-    print(f"Response status {response.status_code}")
-        
+
 if __name__ == '__main__':
     main(auto_envvar_prefix="NR")
